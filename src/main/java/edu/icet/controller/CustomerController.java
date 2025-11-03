13f3048166286bcb87079b067dc896ea35249586
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-03 16:30:34 +0530

    create datasorces to mysql

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
@@ -28,9 +31,20 @@ public class CustomerController {
                 75000.00
         );
     }
+
+    List<Customer> customerList = new ArrayList<>();
+
+    public List<Customer> getAll(){
+        customerList.add( new Customer("1",
+                        "saman",
+                        "walana",
+                        75000.0)
+        );
+        return customerList;
+    }
 }
 
 
 
 
-//http://localhost:8080/swagger-ui/index.html
+// http://localhost:8080/swagger-ui/index.html
